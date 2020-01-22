d22c0ae40c14826cc210720b7e40c24ee82f3a1f
alias-gui <60140039+alias-gui@users.noreply.github.com>
2020-01-22 17:07:07 +0000

    Create rg_ree.f90

---
+++ rg_ree.f90
@@ -0,0 +1,9 @@
+program main
+    
+    real, dimension (:), allocatable :: X, Y, Z, Rg, Ree, Rg2, Ree2
+    integer, dimension (:), allocatable :: NID, NTYP
+    real :: rc, dr, avRg, avRee
+
+    integer :: I, K, P, NDUMP, NS, NB, NP
+    
+end program main
